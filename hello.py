9bb1c921b90d1e1c22856c60f4e460f045f25336
Mart.Valja <Mart@laptop>
2015-09-25 13:22:00 +0300

    Added routes.

--- hello.py
+++ hello.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
+    return 'Hello World!'
+
+@app.route('/resource')
+def resource():
+     print request.args['tala']
+     return "Requested: " + request.args['tala']
+
+@app.route('/resourcereply')
+def resourcereply():
+    return 'Hello World!'
+
+@app.route('/checkmd5')
+def checkmd5():
+    return 'Hello World!'
+
+@app.route('/answermd5')
+def answermd5():
     return 'Hello World!'
 
 if __name__ == '__main__':
