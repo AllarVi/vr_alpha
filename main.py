433d8f4824546532ea565d76cb26221d491a37e5
Mart.Valja <Mart@laptop>
2015-09-26 22:37:11 +0300

    Turned on debugging and added skeleton of vra_resource.

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from flask import Flask, request
+import vra_resource
+
 app = Flask(__name__)
+
 
 
 @app.route('/')
 def hello_world():
+    # TO-DO:
+    # UI for end-user inserting MD5 to crack.
     return 'Hello World!'
 
-@app.route('/resource')
+@app.route('/resource', methods=['GET', 'POST'])
 def resource():
-     print request.args['tala']
-     return "Requested: " + request.args['tala']
+     # Read values from request. Supports both GET and POST, whichever is sent.
+     sendip = request.values.get('sendip')
+     sendport = request.values.get('sendport')
+     ttl = request.values.get('ttl')
+     id = request.values.get('id')
+     noask = request.values.getlist('noask')
+     return vra_resource.resource_handler(sendip, sendport, ttl, id, noask)
 
 @app.route('/resourcereply')
 def resourcereply():
@@ -24,4 +34,5 @@ def answermd5():
     return 'Hello World!'
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
---
+++ vra_resource.py
@@ -0,0 +1,5 @@
+__author__ = 'Mart'
+
+
+def resource_handler(sendip, sendport, ttl, id, noask):
+    return "TO-DO: Return valid response and send requests to all other computers/programs not in noask list."
