623c1068db582688d165887c8a2d05f600ea96fa
Mart <martvalja@gmail.com>
2015-10-07 17:24:41 +0300

    GET sending done (vra_request), request forwarding done

--- vra_resource.py
+++ vra_resource.py
@@ -2,16 +2,23 @@ __author__ = 'Mart'
 
 from vra_io import load_hosts
 import urllib2
-import threading
+import socket
+from flask import request
 
 
 def resource_handler(sendip, sendport, ttl, id, noask):
     # Check if I am busy and respond accordingly
+    response_url = "http://" + sendip + ":" + sendport + "/resourcereply" + "TODO:myData etc as POST data in JSON"
 
+    #Forwarding request
+    # Check if TTL is a digit
+    if ttl.isdigit():
+        ttl = int(ttl)
+    else:
+        return "TTL not a digit, will not forward this request."
 
-    # If TTL > 1, send it to known hosts not in noask list
-    ttl = int(ttl)
-    if (ttl > 1):
+    # If TTL > 0, send it to known hosts not in noask list
+    if (ttl > 0):
         ttl -= 1
 
         # If sender is not in noask list, add it to noask list
@@ -19,20 +26,30 @@ def resource_handler(sendip, sendport, ttl, id, noask):
         sender = [sendip, sendport]
         if (sender not in noask):
             noask.append(sender)
+
+        # Add myself to noask:
+        my_host = str(request.host)
+        my_host_separator_index = my_host.index(':')
+        noask.append([my_host[:my_host_separator_index], my_host[my_host_separator_index + 1:]])
+
         # Check noask list vs known hosts list and generate list of new unique hosts to forward request to
         known_hosts_from_file = load_hosts()
-        noask = [x for x in known_hosts_from_file if x not in noask]
-        # Send request to each unique host
-        # Build request
-        request_variables = [sendip, sendport, ttl, id, noask]
-        # THIS WILL LEAVE THREADS OPEN: threading.Thread(target=urllib2.urlopen, args=("http://127.0.0.1:5000/test",)).start()
+        will_ask = [x for x in known_hosts_from_file if x not in noask]
 
+        # Set up request url
+        my_params = "/resource?sendip=" + str(sendip) + "&sendport=" + str(sendport) + "&ttl=" + str(ttl) + "&id=" + str(id)
+        for host in noask:
+            my_params += "&noask=" + str(host[0]) + "_" + str(host[1])
 
-
-
-    return "Loaded list " + str(load_hosts()) + str(" Noask list:") + str(noask)
-    #return "TO-DO: Return valid response and send requests to all other computers/programs not in noask list."
-
+        # Send requests to each unique host
+        for host in will_ask:
+            try:
+                urllib2.urlopen("http://" + str(host[0]) + ":" + str(host[1]) + my_params, timeout=0.0000001)
+            except socket.error:
+                print "Socket timeout error as expected."
+            except urllib2.URLError as e:
+                print "URLError: " + str(e)
+    return "Noask list:" + str(noask)
 
 def noask_string_to_list(noask):
     noask_list = []
