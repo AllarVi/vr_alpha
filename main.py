3c0e72e0c8202c7a59b73ed8a60c00707f1e686a
aglo35 <allar.viinamae@hotmail.com>
2015-10-09 19:52:29 +0300

    Added unique id generation for client machines

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 # coding=utf-8
-from flask import Flask, request, render_template, url_for, jsonify
+import json
+import socket
+from flask import Flask, request, render_template
 import vra_resource
 import getopt, sys
 import requests
+import uuid
 
 app = Flask(__name__)
 
+potentialWorkers = []
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         if request.form['submit'] == 'sendResourceRequests':
+            # generate random ID for client
+            uid = str(uuid.uuid1())
+
             try:
-                requests.get('http://127.0.0.1:5001/resource?sendip=127.0.0.1&sendport=5000&ttl=10', timeout=0.001)
+                requests.get('http://127.0.0.1:5001/resource?sendip=127.0.0.1&sendport=5000&ttl=10&id=' + uid, timeout=0.001)
             except requests.exceptions.ConnectTimeout as e:
                 print "Too slow Mojo!"
             except requests.exceptions.ConnectionError as e:
                 print "These aren't the domains we're looking for."
             except requests.exceptions.ReadTimeout as e:
                 print "Waited too long between bytes."
+            except socket.error:
+                print "Socket timeout error as expected."
 
             return render_template('form_submit.html')
 
@@ -27,8 +36,11 @@ def index():
 
 @app.route("/resourcereply", methods=['POST'])
 def resourcereply():
+    global potentialWorkers
     # This is to test the data that we got
     print("Data input to /resourcereply: " + str(request.get_data()))
+
+    # potentialWorkerData = json.loads(request.get_data())
 
     # string=request.form['yourstring']
     # return render_template('form_action.html', string=string)
