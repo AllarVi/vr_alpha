229e35bf972d0bcd32628a85e52ad015c10b6a26
Mart <martvalja@gmail.com>
2015-10-10 17:14:52 +0300

    Made MD5 package

--- vra_resource.py
+++ vra_resource.py
@@ -12,8 +12,8 @@ import vra_http_request_helper
 
 def resource_handler(sendip, sendport, ttl, id, noask):
     # Check if I am busy and respond accordingly
-    my_ip = vra_http_request_helper.get_host_ip()
-    my_port = vra_http_request_helper.get_host_port()
+    my_ip = vra_http_request_helper.get_my_ip()
+    my_port = vra_http_request_helper.get_my_port()
     jdata = json.dumps({"ip":my_ip, "port":my_port, "id":id, "resource":str(100)})
     vra_http_request_helper.send_post_request(sendip, sendport, jdata)
 
