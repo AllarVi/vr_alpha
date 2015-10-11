222791ed3f42a9681624152d9405cb085c5a922c
Mart <martvalja@gmail.com>
2015-10-11 15:58:15 +0300

    Changed number of wildcards in range from 2 to 3

--- md5/vra_range_generator.py
+++ md5/vra_range_generator.py
@@ -13,35 +13,38 @@ def get_range(index, wildcard):
     if START_CHAR_ASCII_CODE <= wildcard_ascii_index <= END_CHAR_ASCII_CODE:
         number_of_chars -= 1
 
-    # ? and ??
+    # ? and ?? and ???
     if index is 0:
         range1 = wildcard_string
         range2 = wildcard_string + wildcard_string
-        return [range1, range2]
-    # a??, b??, c?? and d?? and so on.
+        range3 = wildcard_string + wildcard_string + wildcard_string
+        return [range1, range2, range3]
+    # a???, b???, c??? and d??? and so on.
     elif index <= number_of_chars:
-        return [get_char(int(START_CHAR_ASCII_CODE + (index - 1)), wildcard_ascii_index) + wildcard_string + wildcard_string]
-    # aa??, ab??, ac??, ad??, ...
+        return [get_char(int(START_CHAR_ASCII_CODE + (index - 1)), wildcard_ascii_index) +
+                wildcard_string + wildcard_string + wildcard_string]
+    # aa???, ab???, ac???, ad???, ...
     # Must iterate elemnr*elemnr times to cover all,
     # but we have already covered number_of_chars times, so add this.
     elif index <= (number_of_chars**2 + number_of_chars):
         operator = index - number_of_chars - 1
         return [get_char(START_CHAR_ASCII_CODE + int(operator / number_of_chars), wildcard_ascii_index)
-               + get_char(START_CHAR_ASCII_CODE + operator % number_of_chars ,wildcard_ascii_index) + wildcard_string + wildcard_string]
-    # aaa??, aab??, aac??, ...
+               + get_char(START_CHAR_ASCII_CODE + operator % number_of_chars ,wildcard_ascii_index) +
+                wildcard_string + wildcard_string + wildcard_string]
+    # aaa???, aab???, aac???, ...
     elif index <= (number_of_chars**3 + (number_of_chars**2 + number_of_chars)):
         operator = index - (number_of_chars**2 + number_of_chars) - 1
         return [get_char(START_CHAR_ASCII_CODE + int(operator / number_of_chars**2), wildcard_ascii_index) +
                 get_char(START_CHAR_ASCII_CODE + int(operator / number_of_chars) % number_of_chars, wildcard_ascii_index) +
                 get_char(START_CHAR_ASCII_CODE + operator % (number_of_chars), wildcard_ascii_index) +
-                wildcard_string + wildcard_string]
-    # aaa???, aab???, aac???, aad???, ...
+                wildcard_string + wildcard_string + wildcard_string]
+    # aaa????, aab????, aac????, aad????, ...
     elif index <= number_of_chars**3 + (number_of_chars**3 + (number_of_chars**2 + number_of_chars)):
         operator = index - (number_of_chars**2 + number_of_chars) - 1
         return [get_char(START_CHAR_ASCII_CODE + int(operator / number_of_chars**2) % number_of_chars, wildcard_ascii_index) +
                 get_char(START_CHAR_ASCII_CODE + int(operator / number_of_chars) % number_of_chars, wildcard_ascii_index) +
                 get_char(START_CHAR_ASCII_CODE + operator % (number_of_chars), wildcard_ascii_index) +
-                wildcard_string + wildcard_string + wildcard_string]
+                wildcard_string + wildcard_string + wildcard_string + wildcard_string]
     return []
 
 
