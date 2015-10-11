c61ab2b4e79e036226fc65cdf37d6aab8d36555e
Mart <martvalja@gmail.com>
2015-10-11 16:38:36 +0300

    Tidied the code a bit.

--- md5/vra_range_generator.py
+++ md5/vra_range_generator.py
@@ -1,12 +1,13 @@
 __author__ = 'Mart'
 
 START_CHAR_ASCII_CODE = 32
-END_CHAR_ASCII_CODE = 125
+END_CHAR_ASCII_CODE = 126
 
 #TODO: ATM it returns ranges where there are wildcards also included
 # (etc: wildcard = _ and it returns a_b___ though pos1 is not a wildcard
 
 def get_range(index, wildcard):
+    """Returns range according to int index and wildcard. Ranges are """
     wildcard_string = str(wildcard)
     wildcard_ascii_index = ord(wildcard)
     number_of_chars = END_CHAR_ASCII_CODE - START_CHAR_ASCII_CODE + 1
@@ -24,8 +25,6 @@ def get_range(index, wildcard):
         return [get_char(int(START_CHAR_ASCII_CODE + (index - 1)), wildcard_ascii_index) +
                 wildcard_string + wildcard_string + wildcard_string]
     # aa???, ab???, ac???, ad???, ...
-    # Must iterate elemnr*elemnr times to cover all,
-    # but we have already covered number_of_chars times, so add this.
     elif index <= (number_of_chars**2 + number_of_chars):
         operator = index - number_of_chars - 1
         return [get_char(START_CHAR_ASCII_CODE + int(operator / number_of_chars), wildcard_ascii_index)
@@ -40,34 +39,47 @@ def get_range(index, wildcard):
                 wildcard_string + wildcard_string + wildcard_string]
     # aaa????, aab????, aac????, aad????, ...
     elif index <= number_of_chars**3 + (number_of_chars**3 + (number_of_chars**2 + number_of_chars)):
-        operator = index - (number_of_chars**2 + number_of_chars) - 1
-        return [get_char(START_CHAR_ASCII_CODE + int(operator / number_of_chars**2) % number_of_chars, wildcard_ascii_index) +
-                get_char(START_CHAR_ASCII_CODE + int(operator / number_of_chars) % number_of_chars, wildcard_ascii_index) +
-                get_char(START_CHAR_ASCII_CODE + operator % (number_of_chars), wildcard_ascii_index) +
-                wildcard_string + wildcard_string + wildcard_string + wildcard_string]
-    return []
+        indexer = 1
+    # aaa?????, aab?????, aac?????, aad?????, ...
+    elif index <= number_of_chars**3 + (number_of_chars**3 + (number_of_chars**3 + (number_of_chars**2 + number_of_chars))):
+        indexer = 2
+    # aaa??????, aab??????, aac??????, aad??????, ...
+    elif index <= number_of_chars**3 + number_of_chars**3 + (number_of_chars**3 + (number_of_chars**3 + (number_of_chars**2 + number_of_chars))):
+        indexer = 3
+    else:
+        return []
+
+    operator = index - (indexer * number_of_chars**3 + number_of_chars**2 + number_of_chars) - 1
+    return [get_char(START_CHAR_ASCII_CODE + int(operator / number_of_chars**2) % number_of_chars, wildcard_ascii_index) +
+            get_char(START_CHAR_ASCII_CODE + int(operator / number_of_chars) % number_of_chars, wildcard_ascii_index) +
+            get_char(START_CHAR_ASCII_CODE + operator % (number_of_chars), wildcard_ascii_index) +
+            wildcard_string * (3 + indexer)]
 
 
 def get_ranges(startindex, lastindex, wildcard):
+    """Returns multiple ranges in an array"""
     i = startindex
     ranges = []
     while i <= lastindex:
         ranges += get_range(i,wildcard)
-        print("Ranges(" + str(i) + "): "+ str(get_range(i,wildcard)))
         i += 1
     return ranges
 
 
 def get_char(char_ascii_code, wildcard_ascii_code):
-    if char_ascii_code < wildcard_ascii_code:
+    """Returns character after checking that said char is not a wildcard."""
+    # Wildcard comes after char or wildcard before range begins
+    if char_ascii_code < wildcard_ascii_code or wildcard_ascii_code < START_CHAR_ASCII_CODE:
         return chr(char_ascii_code)
+    # Char between wildcard and end. Cannot be equal because of +1, would be out of range.
     elif char_ascii_code < END_CHAR_ASCII_CODE:
         return chr(char_ascii_code + 1)
     else:
-        # workaround for char_ascii_code == END_CHAR_ASCII_CODE + 1, at least makes sure that no wildcard is inserted.
+        # workaround for char_ascii_code == END_CHAR_ASCII_CODE + 1,
+        # at least makes sure that no wildcard is inserted.
         # Actual code should never enter here.
         print("Error: OUT_OF_RANGE vra_range_generator.get_char()")
-        if wildcard_ascii_code is not ord("a"):
-            return "a"
+        if wildcard_ascii_code is not ord("Z"):
+            return "Z"
         else:
-            return "b"
+            return "z"
